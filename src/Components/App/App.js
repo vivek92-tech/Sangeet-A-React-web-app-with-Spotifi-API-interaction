b18bcf25d49bb4ed6f292450aa6d1f2421cddeeb
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-08-16 15:56:52 -0400

    Implementing Spotify Search results- created a method that accept search term input, passes the search term value to a Spotify request, then returns the response as a list of tracks in JSON format.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+
 
 class App extends React.Component{
   constructor(props){
@@ -55,7 +57,9 @@ class App extends React.Component{
   }
 
   search(term){
-      console.log(term);
+      Spotify.search(term).then(searchResults => {
+        this.setState({searchResults: searchResults})
+      }) 
   }
 
 
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -31,7 +31,35 @@ const Spotify = {
         }
 
 
+    },
+
+    search(term){
+        const accessToken = Spotify.getAccessToken();
+
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+        {headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+     if (!jsonResponse.tracks){
+         return [];
+     }
+     else {
+         return jsonResponse.tracks.items.map(track => ({
+            id: track.id,
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri
+         }));
+     }
+
+
+    });
     }
+
 };
 
 
