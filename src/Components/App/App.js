1e2a896687b16fe9aa2fd41d0d57e4281caaea79
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-08-15 23:26:11 -0400

    Add Tracks to a Playlist- added a method addTrack that adds songs to the playlist state.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,7 +22,20 @@ class App extends React.Component{
       ]
 
     }
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id===track.id)){
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+    
+  }
+
+
 
   render(){
     return (
@@ -31,7 +44,8 @@ class App extends React.Component{
       <div className="App">    
         <SearchBar />
         <div className="App-playlist">
-               <SearchResults searchResults={this.state.searchResults}/>
+               <SearchResults searchResults={this.state.searchResults}
+                              onAdd={this.addTrack} />
                 <Playlist playlistName={this.state.playlistName}
                           playlistTracks={this.state.playlistTracks}/>
         </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component{
+    constructor (props){
+        super(props);
+        //this.renderAction = this.renderAction.bind(this);
+        this.addTrack=this.addTrack.bind(this);
+    }
+
+
     renderAction(){
             if (this.props.isRemoval){
                    return <button className="Track-action">-</button> 
             }
             else{
-                return <button className="Track-action">+</button>
+                return <button className="Track-action" onClick={this.addTrack}>+</button>
             }
+    }
+    addTrack(){
+        this.props.onAdd(this.props.track);
     }
     render(){
         return(
